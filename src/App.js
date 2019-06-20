81132469d60d529397b4da7bdd3a37948d4c7d8c
Jonathan Petrinolis <jpetrinolis@gmail.com>
2019-06-20 21:38:07 +0930

    added material-ui for slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Slider from './components/Slider';
+
 import './App.css';
+class App extends Component {
+state = {
+  slider: {
+    value: [2, 4]
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Slider value={this.state.slider.value}/>
+      </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/components/Slider.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import SliderUI from '@material-ui/lab/Slider';
+
+
+export class Slider extends Component {
+
+  handleChange = (event, newValue) => {
+    console.log(newValue);
+  };
+
+  render() {
+
+    return (
+      <div>
+        <SliderUI
+          value={this.props.value}
+          min={0}
+          max={5}
+          onChange={this.handleChange}
+          valueLabelDisplay="auto"
+        />
+      </div>
+    )
+  };
+}
+
+export default Slider; 
