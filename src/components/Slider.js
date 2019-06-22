7eaaca428a253d6589ff7bc26396fb88758e1655
Jonathan Petrinolis <jpetrinolis@gmail.com>
2019-06-22 17:18:29 +0930

    created sliders

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,25 +3,43 @@ import SliderUI from '@material-ui/lab/Slider';
 
 
 class Slider extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      rating: [0, 5],
+      cost: [0, 5],
+    }
+  }
 
-  handleChange = (event, newValue) => {
-    console.log(newValue);
-  };
+  handleChange = (name, event, newValue) => {
+    this.setState({ [name]: newValue });
+  }
 
   render() {
 
     return (
-      <div>
-        <SliderUI
-          value={this.props.value}
-          min={0}
-          max={5}
-          onChange={this.handleChange}
-          valueLabelDisplay="auto"
-        />
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{ height: '100px', width: '300px'}} >
+          <SliderUI
+            value={this.state.rating}
+            min={0}
+            max={5}
+            onChange={this.handleChange.bind(this, 'rating')}
+            valueLabelDisplay="auto"
+          />
+        </div>
+        <div style={{ height: '100px', width: '300px'}}>
+          <SliderUI
+            value={this.state.cost}
+            min={0}
+            max={5}
+            onChange={this.handleChange.bind(this, 'cost')}
+            valueLabelDisplay="auto"
+          />
+        </div>
       </div>
     )
-  };
+  }
 }
 
 export default Slider; 
